94f971f5b33f00b07d01d2787855f6eed6fd5363
Chirag Jain <jain_chirag04@yahoo.com>
2017-04-08 20:10:11 -0400

    back handler

--- src/ExNavigationBackButtonManager.js
+++ src/ExNavigationBackButtonManager.js
@@ -2,7 +2,7 @@
  * @flow
  */
 
-import { BackAndroid } from 'react-native';
+import { BackHandler } from 'react-native';
 
 import ExNavigationActions from './ExNavigationActions';
 
@@ -12,7 +12,7 @@ import type { ExNavigationStore } from './ExNavigationStore';
  * Manages a global listener, as well as any custom listeners, on the
  * Android hardware back button.
  *
- * Rather than using the BackAnroid React Native module directly, use this
+ * Rather than using the BackHandler React Native module directly, use this
  * class to manage the any custom listeners or to enable or disable the
  * global back button listener.
  */
@@ -54,8 +54,8 @@ class ExNavigationBackButtonManager {
   }
 
   disable() {
-    this._listeners.forEach(listener => BackAndroid.removeEventListener('hardwareBackPress', listener));
-    BackAndroid.addEventListener('hardwareBackPress', this._disabledBackButtonPress); // Don't let app be exited.
+    this._listeners.forEach(listener => BackHandler.removeEventListener('hardwareBackPress', listener));
+    BackHandler.addEventListener('hardwareBackPress', this._disabledBackButtonPress); // Don't let app be exited.
   }
 
   enable() {
@@ -65,8 +65,8 @@ class ExNavigationBackButtonManager {
   _setListeners(newListeners: Array<() => Promise<void>>) {
     this.disable();
     this._listeners = newListeners;
-    BackAndroid.removeEventListener('hardwareBackPress', this._disabledBackButtonPress);
-    BackAndroid.addEventListener('hardwareBackPress', this._listeners[this._listeners.length - 1]);
+    BackHandler.removeEventListener('hardwareBackPress', this._disabledBackButtonPress);
+    BackHandler.addEventListener('hardwareBackPress', this._listeners[this._listeners.length - 1]);
   }
 
   _onHardwareBackPress = async () => {
@@ -75,7 +75,7 @@ class ExNavigationBackButtonManager {
     }
     const moreRoutes = await this._store.dispatch(ExNavigationActions.goBack());
     if (moreRoutes === false) {
-      BackAndroid.exitApp();
+      BackHandler.exitApp();
     }
   };
 
